e27aa3277234a237526ed0e82671b77837d335a0
Daniel Larsen <daniel@larsen.net.nz>
2015-04-12 10:54:43 +1200

    First cut of Blob Storage support

---
+++ Scale.Storage/Blob/AzureBlobStorage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace Scale.Storage.Blob
+{
+    public class AzureBlobStorage : AzureStorage, IBlobStorage
+    {
+        // http://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/
+
+        private CloudBlobClient _blobClient;
+
+        /// <summary>
+        /// Instantiates a new <see cref="AzureTableStorage"/>.
+        /// </summary>
+        /// <param name="settings"></param>
+        public AzureBlobStorage(NameValueCollection settings)
+            : base(settings)
+        {
+            _blobClient = StorageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task CreateContainer(string containerName)
+        {
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = _blobClient.GetContainerReference(containerName);
+
+            // Create the container if it doesn't already exist.
+            await container.CreateIfNotExistsAsync();
+        }
+
+        public async Task<string> Upload(string containerName, string blobName, Stream stream)
+        {
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = _blobClient.GetContainerReference(containerName);
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+            await blockBlob.UploadFromStreamAsync(stream);
+            return blockBlob.Uri.ToString();
+        }
+
+        public async Task<Stream> Download(string uri)
+        {
+            var blob = await _blobClient.GetBlobReferenceFromServerAsync(new Uri(uri));
+
+            //// Retrieve reference to a previously created container.
+            //CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            //// Retrieve reference to a blob named "photo1.jpg".
+            //CloudBlockBlob blockBlob = container.GetBlockBlobReference("photo1.jpg");
+
+            // Download to memory stream
+            using (var stream = new System.IO.MemoryStream())
+            {
+                await blob.DownloadToStreamAsync(stream);
+                return stream;
+            }
+        }
+    }
+}
---
+++ Scale.Storage/Blob/IBlobStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Scale.Storage.Blob
+{
+    public interface IBlobStorage
+    {
+        Task CreateContainer(string containerName);
+
+        Task<string> Upload(string containerName, string blobName, Stream stream);
+
+		Task<Stream> Download(string uri);
+    }
+}
